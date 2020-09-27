eccb8f31251f5eae6c8327ad267329d8c711ce3a
savimittal02 <71250413+savimittal02@users.noreply.github.com>
2020-09-27 12:17:44 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,49 @@
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('bedroom.jpg');
+}
+
+
+function setup() {
+  canvas = createCanvas(540, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!");
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(img, 0, 0, 640,420);
+
+      if(status != "")
+      {
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+          fill("red");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x+15 , objects[i].y+20 );
+          noFill();
+          stroke("blue");
+          strokeWeight(2);
+          rect(objects[i].x-90, objects[i].y-30, objects[i].width, objects[i].height);
+        }
+      }
+}
